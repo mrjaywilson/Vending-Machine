9d6580aeb929c6a07ddc821f126b7fb2a6a67f91
Jay <mrjaywilson@live.com>
2018-06-14 13:12:13 -0700

    Update VendoMatic.java

--- src/com/mrjaywilson/vendomatic/main/VendoMatic.java
+++ src/com/mrjaywilson/vendomatic/main/VendoMatic.java
@@ -1,11 +1,8 @@
 /**
-* 	Program:	Vend-o-Matic (Vending Machine Project)
-*	File:		Vendomatic.java
-*	Summary:	Main program.
-* 	Author:		Jay Wilson
-* 				Bibata RABBA IDI
-*	Date:		May 04, 2018
-**/
+* Vendomatic.java - Main Entry-point for program
+* @author  Jay Wilson
+* @version 1.0
+*/
 
 // Declare package
 package com.mrjaywilson.vendomatic.main;

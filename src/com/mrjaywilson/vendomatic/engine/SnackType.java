4e6443d1547066be851428c5b219fd13258ea6bc
Jay <mrjaywilson@live.com>
2018-06-14 12:43:35 -0700

    Update SnackType.java

--- src/com/mrjaywilson/vendomatic/engine/SnackType.java
+++ src/com/mrjaywilson/vendomatic/engine/SnackType.java
@@ -1,12 +1,3 @@
-/**
-* 	Program:	Vend-o-Matic (Vending Machine Project)
-*	File:		SnackType.java
-*	Summary:	Manages SnackType enum.
-* 	Author:		Jay Wilson
-* 				Bibata RABBA IDI
-*	Date:		May 04, 2018
-**/
-
 // Package declaration
 package com.mrjaywilson.vendomatic.engine;
 

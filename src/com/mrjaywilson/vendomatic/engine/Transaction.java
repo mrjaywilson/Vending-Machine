2ee5a9be2ced8e2b73edba37f5906fbcd0f3a210
Jay <mrjaywilson@live.com>
2018-06-14 12:43:53 -0700

    Update Transaction.java

--- src/com/mrjaywilson/vendomatic/engine/Transaction.java
+++ src/com/mrjaywilson/vendomatic/engine/Transaction.java
@@ -1,12 +1,3 @@
-/**
-* 	Program:	Vend-o-Matic (Vending Machine Project)
-*	File:		Transactions.java
-*	Summary:	Manages Transactions.
-* 	Author:		Jay Wilson
-* 				Bibata RABBA IDI
-*	Date:		May 04, 2018
-**/
-
 // Declare Package
 package com.mrjaywilson.vendomatic.engine;
 

18e15981d123745c7bcd27c20c4deb68bc307fa6
Jay <mrjaywilson@live.com>
2018-06-14 12:46:44 -0700

    Update Drink.java

--- src/com/mrjaywilson/vendomatic/inventory/Drink.java
+++ src/com/mrjaywilson/vendomatic/inventory/Drink.java
@@ -1,12 +1,3 @@
-/**
-* 	Program:	Vend-o-Matic (Vending Machine Project)
-*	File:		Drink.java 
-*	Summary:	Manages drink objects.
-* 	Author:		Jay Wilson
-* 				Bibata RABBA IDI
-*	Date:		May 04, 2018
-**/
-
 // package declaration 
 package com.mrjaywilson.vendomatic.inventory;
 
